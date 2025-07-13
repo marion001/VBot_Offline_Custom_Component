dc8e832f6f8905c7b5a3b4f0af55bb584d980426
marion001 <mr.marion001@gmail.com>
2025-07-14 01:13:39 +0700

    13/07/2025

--- custom_components/vbot_assistant_mqtt/__init__.py
+++ custom_components/vbot_assistant_mqtt/__init__.py
@@ -3,18 +3,18 @@ from .const import DOMAIN
 async def async_setup(hass, config):
     return True
 
+#Thêm các platform entry_id cần dùng
 async def async_setup_entry(hass, entry):
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
-    #Thêm các platform entry_id cần dùng
     await hass.config_entries.async_forward_entry_setups(
         #entry, ["switch", "number", "sensor", "select", "script", "input_text"]
         entry, ["switch", "number", "sensor", "select", "button", "input_text_entity"]
     )
     return True
 
+#Gỡ tất cả các platform entry_id đã thêm vào trước đó
 async def async_unload_entry(hass, entry):
-    #Gỡ tất cả các platform entry_id đã thêm vào trước đó
     await hass.config_entries.async_unload_platforms(
         #entry, ["switch", "number", "sensor", "select", "script", "input_text"]
         entry, ["switch", "number", "sensor", "select", "button", "input_text_entity"]
