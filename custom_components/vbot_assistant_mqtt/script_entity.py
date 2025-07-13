322ad94cf84ea463fc4ef74a5a3d799b9db0cf14
marion001 <mr.marion001@gmail.com>
2025-07-13 23:28:26 +0700

    13/07/2025

--- custom_components/vbot_assistant_mqtt/script_entity.py
+++ custom_components/vbot_assistant_mqtt/script_entity.py
@@ -1,13 +1,13 @@
-import logging
-from homeassistant.helpers.entity import Entity
-from homeassistant.components import mqtt
+from homeassistant.components.button import ButtonEntity
+from homeassistant.components.mqtt import async_publish
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from .const import DOMAIN
 
-_LOGGER = logging.getLogger(__name__)
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback):
+    device = entry.data.get("device_id", "UNKNOWN_DEVICE")
 
-async def async_setup_entry(hass, entry, async_add_entities):
-    config = entry.data
-    device = config.get("device_id", "VBot")
-    
     scripts = [
         {
             "id": f"{device}_media_pause",
@@ -17,58 +17,37 @@ async def async_setup_entry(hass, entry, async_add_entities):
             "payload": "PAUSE"
         },
         {
-            "id": f"{device}_media_stop",
-            "name": f"{device} Media Stop",
-            "icon": "mdi:stop-circle-outline",
-            "topic": f"{device}/script/media_control/set",
-            "payload": "STOP"
-        },
-        {
             "id": f"{device}_vbot_tts",
             "name": f"{device} VBot TTS",
             "icon": "mdi:robot",
             "topic": f"{device}/script/vbot_tts/set",
             "template_input": f"input_text.{device.lower()}_vbot_tts"
         },
-        {
-            "id": f"{device}_main_processing",
-            "name": f"{device} Main Processing",
-            "icon": "mdi:robot-confused-outline",
-            "topic": f"{device}/script/main_processing/set",
-            "template_input": f"input_text.{device.lower()}_main_processing"
-        }
     ]
 
-    entities = [MQTTScriptEntity(hass, script) for script in scripts]
+    entities = [VBotScriptButton(hass, **conf) for conf in scripts]
     async_add_entities(entities)
 
-
-class MQTTScriptEntity(Entity):
-    def __init__(self, hass, config):
+class VBotScriptButton(ButtonEntity):
+    def __init__(self, hass: HomeAssistant, id, name, icon, topic, payload=None, template_input=None):
         self._hass = hass
-        self._id = config["id"]
-        self._attr_unique_id = config["id"]
-        self._attr_name = config["name"]
-        self._attr_icon = config.get("icon", "mdi:script")
-        self._topic = config["topic"]
-        self._payload = config.get("payload")
-        self._input_text = config.get("template_input")
+        self._attr_unique_id = id
+        self._attr_name = name
+        self._attr_icon = icon
+        self._topic = topic
+        self._payload = payload
+        self._template_input = template_input
 
-    async def async_turn_on(self, **kwargs):
+    async def async_press(self) -> None:
         payload = self._payload
-        if self._input_text:
-            state_obj = self._hass.states.get(self._input_text)
-            if state_obj:
-                payload = state_obj.state
+        if self._template_input:
+            state = self._hass.states.get(self._template_input)
+            payload = state.state if state else ""
 
-        if payload is not None:
-            _LOGGER.debug(f"[{self._attr_name}] Publish to {self._topic}: {payload}")
-            await mqtt.async_publish(self._hass, self._topic, payload, qos=1, retain=True)
-
-    @property
-    def should_poll(self):
-        return False
-
-    @property
-    def available(self):
-        return True
+        await async_publish(
+            self._hass,
+            self._topic,
+            payload,
+            qos=1,
+            retain=True
+        )
