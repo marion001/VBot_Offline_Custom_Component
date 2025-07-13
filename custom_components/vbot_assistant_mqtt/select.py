03c88a84293f57c5204370720ccd474594d3ad48
Marion001 <mr.marion001@gmail.com>
2025-07-13 21:23:30 +0700

    13/07/2025

---
+++ custom_components/vbot_assistant_mqtt/select.py
@@ -0,0 +1,76 @@
+import logging
+from homeassistant.components.select import SelectEntity
+from homeassistant.components import mqtt
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from .const import DOMAIN, CONF_DEVICE_ID
+
+_LOGGER = logging.getLogger(__name__)
+
+async def async_setup_platform(hass: HomeAssistant, config, async_add_entities, discovery_info=None):
+    pass  # Not used
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
+    cfg = entry.data
+    device = cfg[CONF_DEVICE_ID]
+
+    selects = [
+        {
+            "name": f"{device} Kiểu Hiển Thị Logs",
+            "state_topic": f"{device}/select/log_display_style/state",
+            "command_topic": f"{device}/select/log_display_style/set",
+            "options": ["console", "display_screen", "api", "all"],
+            "icon": "mdi:math-log"
+        }
+    ]
+
+    entities = [MQTTSelect(hass, **s) for s in selects]
+    async_add_entities(entities, update_before_add=True)
+
+
+class MQTTSelect(SelectEntity):
+    def __init__(self, hass, name, state_topic, command_topic, options, icon):
+        self._hass = hass
+        self._name = name
+        self._state_topic = state_topic
+        self._command_topic = command_topic
+        self._options = options
+        self._attr_icon = icon
+        self._state = None
+
+    async def async_added_to_hass(self):
+        await mqtt.async_subscribe(
+            self._hass,
+            self._state_topic,
+            self._message_received,
+            qos=1
+        )
+
+    async def _message_received(self, msg):
+        payload = msg.payload
+        _LOGGER.debug(f"{self._name} MQTT recv: {payload}")
+        self._state = payload
+        self.async_write_ha_state()
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def options(self):
+        return self._options
+
+    @property
+    def current_option(self):
+        return self._state
+
+    async def async_select_option(self, option):
+        await mqtt.async_publish(
+            self._hass,
+            self._command_topic,
+            option,
+            qos=1,
+            retain=False
+        )
+        self._state = option  # Nếu optimistic
+        self.async_write_ha_state()
---
+++ custom_components/vbot_assistant_mqtt/sensor.py
@@ -0,0 +1,72 @@
+import logging
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.components import mqtt
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from .const import DOMAIN, CONF_DEVICE_ID
+
+_LOGGER = logging.getLogger(__name__)
+
+async def async_setup_platform(hass: HomeAssistant, config, async_add_entities, discovery_info=None):
+    pass  # Not used
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
+    cfg = entry.data
+    device = cfg[CONF_DEVICE_ID]
+
+    sensors = [
+        {
+            "name": f"{device} Ngày Phát Hành Giao Diện",
+            "state_topic": f"{device}/sensor/vbot_interface_releaseDate/state",
+            "icon": "mdi:calendar"
+        },
+        {
+            "name": f"{device} Phiên Bản Giao Diện",
+            "state_topic": f"{device}/sensor/vbot_interface_version/state",
+            "icon": "mdi:calendar"
+        },
+        {
+            "name": f"{device} Phiên Bản Chương Trình",
+            "state_topic": f"{device}/sensor/vbot_program_version/state",
+            "icon": "mdi:calendar"
+        },
+        {
+            "name": f"{device} Ngày Phát Hành Chương Trình",
+            "state_topic": f"{device}/sensor/vbot_program_releaseDate/state",
+            "icon": "mdi:calendar"
+        },
+    ]
+
+    entities = [MQTTSensor(hass, **s) for s in sensors]
+    async_add_entities(entities, update_before_add=True)
+
+
+class MQTTSensor(SensorEntity):
+    def __init__(self, hass, name, state_topic, icon):
+        self._hass = hass
+        self._name = name
+        self._state_topic = state_topic
+        self._attr_icon = icon
+        self._state = None
+
+    async def async_added_to_hass(self):
+        await mqtt.async_subscribe(
+            self._hass,
+            self._state_topic,
+            self._message_received,
+            qos=1
+        )
+
+    async def _message_received(self, msg):
+        payload = msg.payload
+        _LOGGER.debug(f"{self._name} MQTT recv: {payload}")
+        self._state = payload
+        self.async_write_ha_state()
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def state(self):
+        return self._state
