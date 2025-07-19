82cc5ab949f4e0022e1a3d3bb9b466add02d8e54
marion001 <mr.marion001@gmail.com>
2025-07-19 23:01:24 +0700

    17/07/2025

--- custom_components/vbot_assistant_mqtt/select.py
+++ custom_components/vbot_assistant_mqtt/select.py
@@ -100,11 +100,14 @@ class MQTTSelect(SelectEntity):
 class ProcessingModeSelect(SelectEntity):
     def __init__(self, device):
         self._device = device
-        self._attr_name = f"Chế Độ Xử Lý Tác Nhân Assist ({device})"
+        self._attr_name = f"Assist Tác Nhân Chế Độ Xử Lý ({device})"
         self._attr_unique_id = f"assist_processing_mode_select_{device.lower()}"
         self._attr_options = ["chatbot", "processing"]
         self._attr_icon = "mdi:robot"
         self._attr_current_option = "chatbot"
+        self._attr_entity_id = f"select.assist_processing_mode_select_{device.lower()}"
+
+
 
     @property
     def current_option(self):
