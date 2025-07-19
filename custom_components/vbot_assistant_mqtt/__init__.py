292d5cf264d8a977139a469fcc67c3bd97ebdb97
Marion001 <mr.marion001@gmail.com>
2025-07-19 19:56:20 +0700

    19/07/2025

--- custom_components/vbot_assistant_mqtt/__init__.py
+++ custom_components/vbot_assistant_mqtt/__init__.py
@@ -1,6 +1,5 @@
-from .const import DOMAIN
+from .const import DOMAIN, CONF_DEVICE_ID
 from .assist_pipeline import VBotAssistAgent
-
 
 
 async def async_setup(hass, config):
@@ -13,23 +12,15 @@ async def async_get_tts_engine(hass, config, discovery_info=None):
 async def async_setup_entry(hass, entry):
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
-    
-    # ✅ Đăng ký tác nhân Assist
-    device_id = entry.data.get(CONF_DEVICE_ID)
-    if device_id:
-        agent = VBotAssistAgent(hass, device_id)
-        await hass.components.assist_pipeline.async_register_agent(agent)
-        hass.data.setdefault("assist_pipeline", {})[agent.id] = agent
-    
     await hass.config_entries.async_forward_entry_setups(
-        entry, ["switch", "number", "sensor", "select", "button", "text", "media_player", "assist_pipeline"]
+        entry, ["switch", "number", "sensor", "select", "button", "text", "media_player"]
     )
     return True
 
 #Gỡ tất cả các platform entry_id đã thêm vào trước đó
 async def async_unload_entry(hass, entry):
     await hass.config_entries.async_unload_platforms(
-        entry, ["switch", "number", "sensor", "select", "button", "text", "media_player", "assist_pipeline"]
+        entry, ["switch", "number", "sensor", "select", "button", "text", "media_player"]
     )
     hass.data[DOMAIN].pop(entry.entry_id)
     return True
