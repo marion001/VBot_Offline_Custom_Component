91c25fc2b078284253d7af9872fffc49beb331be
marion001 <mr.marion001@gmail.com>
2025-07-19 21:56:10 +0700

    17/07/2025

--- custom_components/vbot_assistant_mqtt/config_flow.py
+++ custom_components/vbot_assistant_mqtt/config_flow.py
@@ -52,9 +52,6 @@ class VBotConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
 
-# ✅ Cho phép cấu hình lại
-async def async_get_options_flow(self, config_entry):
-    return VBotOptionsFlowHandler(config_entry)
 
 
 # ✅ Giao diện "Cấu hình lại"
@@ -84,3 +81,7 @@ class VBotOptionsFlowHandler(config_entries.OptionsFlow):
         })
 
         return self.async_show_form(step_id="init", data_schema=schema, errors=errors)
+
+# ✅ Cho phép cấu hình lại
+async def async_get_options_flow(self, config_entry):
+    return VBotOptionsFlowHandler(config_entry)
